364cd59f76fe2c3e9f7ccf96d8dbf853e5b169d4
Vitahu <79806896+VitahuHere@users.noreply.github.com>
2024-12-11 09:51:08 +0100

    update

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import joblib
 
 from enum import Enum
 
-import fastapi
-import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.ensemble import RandomForestRegressor
@@ -21,7 +19,7 @@ class SupportedCities(Enum):
 
 class TimeOfDayBucket(Enum):
     EVENING = 'Evening'
-    EARLY_MORNING = 'Early Morning'
+    EARLY_MORNING = 'Early_Morning'
     MORNING = 'Morning'
     AFTERNOON = 'Afternoon'
     NIGHT = 'Night'
@@ -39,11 +37,29 @@ class PredictionInput(BaseModel):
 app = FastAPI()
 
 
+pred_table = {
+    'is_direct': False,
+    'duration': 0,
+    'days_left': 0,
+    'source_city_Bangalore': False,
+    'source_city_Chennai': False,'source_city_Delhi': False,'source_city_Hyderabad': False,
+       'source_city_Kolkata': False,'source_city_Mumbai': False,'departure_time_Afternoon': False,
+       'departure_time_Early_Morning': False,'departure_time_Evening': False,
+       'departure_time_Late_Night': False,'departure_time_Morning': False,
+       'departure_time_Night': False,'arrival_time_Afternoon': False,
+       'arrival_time_Early_Morning': False,'arrival_time_Evening': False,
+       'arrival_time_Late_Night': False,'arrival_time_Morning': False,'arrival_time_Night': False,
+       'destination_city_Bangalore': False,'destination_city_Chennai': False,
+       'destination_city_Delhi': False,'destination_city_Hyderabad': False,
+       'destination_city_Kolkata': False,'destination_city_Mumbai': False,
+}
+
+
 @app.post("/predict")
-def predict():
+def predict(data: PredictionInput):
+    departure_city = f"source_city_{data.source_city}"
+    arrival_city = f"destination_city_{data.destination_city}"
+
     model: RandomForestRegressor = joblib.load("random_forest.pkl")
     scaler: StandardScaler = joblib.load("scaler.pkl")
-    x = np.array([True, 130, 1, False, False, True, False, False, False, False, False, True, False, False, False, False,
-         False, False, False, False, True, False, False, False, False, False, True
-         ]).reshape(1, -1)
-    return scaler.inverse_transform(model.predict(x).reshape(1, -1))
+    return scaler.inverse_transform(model.predict(x).reshape(1,-1))
